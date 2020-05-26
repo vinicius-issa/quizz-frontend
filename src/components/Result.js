d96b5d12648ac98a36b4aa2ee81553e7a3945cc3
Vinicius Issa <vinicius.almeidaissa@gmail.com>
2020-05-26 20:51:25 -0300

    [refact][result]Format screen
    Formatted result screen

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react'
 import { getUser, getToken } from '../services/auth'
 import api from '../services/api'
 
-import {Container} from '@material-ui/core'
+import {Container, Button} from '@material-ui/core'
 
 export default props => {
 
-    const [answers,setAnswers] = useState([]);
+    const [countAnswers,setCountAnswers] = useState(0);
+    const [countCorrects,setCountCorrects] = useState(0);
     const username = getUser();
     const userId = getToken();
 
@@ -14,15 +15,23 @@ export default props => {
         const getAnswers = async () =>{
             let response = await api.get(`question/${userId}/`);
             let responses = response.data['answered'];
-            setAnswers(responses);
+            setCountAnswers(responses.length);
+            let corrects = responses.filter(choice=>choice.is_correct);
+            setCountCorrects(corrects.length)
         }
         getAnswers();
-    });
+    },);
 
     return (
-        <Container>
+        <Container className="center" maxWidth='md'>
             <h1>Resultado de {username}</h1>
-            Voce Acertou {answers.filter(el=>el.is_correct).length} de {answers.length} questões
+            <h3>Voce Acertou {countCorrects} de {countAnswers} questões</h3>
+            O que corresponde a {countCorrects/countAnswers*100}% das respostas
+            <div>
+                <Button href="/signin" variant="contained" color="primary">
+                    Refazer o Quizz
+                </Button>
+            </div>
         </Container>
     )
 
